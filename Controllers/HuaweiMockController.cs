04251d38d6f039ee79e6590ad0df56f85ce270f5
zhouxc <zhouxiaochun_000@163.com>
2020-07-07 08:55:00 +0800

    Add project

---
+++ Controllers/HuaweiMockController.cs
@@ -0,0 +1,115 @@
+
+using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace WebApi.Controllers
+{
+    [ApiController]
+    [Route("tsid/tsidsynservice/external/v1/")]
+    public class HuaweiMockController : ControllerBase
+    {
+        private readonly ILogger<HuaweiMockController> _logger;
+        public HuaweiMockController(ILogger<HuaweiMockController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost]
+        [Route("project/user/query")]
+        public async Task<IActionResult> ProjectQuery(string account)
+        {
+            _logger.LogInformation("Query project, account = {}", account);
+            if (string.IsNullOrWhiteSpace(account))
+                return new JsonResult(new { Error = 404, Msg = "account is empty" });
+
+            using (var textReader = System.IO.File.OpenText("./TestData/project.json"))
+            {
+                using (var jsonReader = new JsonTextReader(textReader))
+                {
+                    return new JsonResult(await JToken.ReadFromAsync(jsonReader));
+                }
+            }
+        }
+
+        [HttpPost]
+        [Route("site/task/query")]
+        public async Task<IActionResult> TaskSiteQuery()
+        {
+            var headers = HttpContext.Request.Headers;
+            var projectCode = headers["project-code"].ToString();
+            var projectId = headers["project-id"].ToString();
+            var userInfo = headers["user-info"].ToString();
+
+            _logger.LogInformation("Query task site, projectCode = {}, projectId = {}, userInfo = {}",
+            projectCode, projectId, userInfo);
+
+            if (string.IsNullOrWhiteSpace(projectCode) ||
+            string.IsNullOrWhiteSpace(projectId) ||
+            string.IsNullOrWhiteSpace(userInfo) ||
+            userInfo.Substring(1, 9) != "\"account\"")
+                return new JsonResult(new { ErrorCode = 404, Msg = "account error" });
+
+            using (var textReader = System.IO.File.OpenText("./TestData/tasksite.json"))
+            {
+                using (var jsonReader = new JsonTextReader(textReader))
+                {
+                    return new JsonResult(await JToken.ReadFromAsync(jsonReader));
+                }
+            }
+        }
+
+        [HttpPost]
+        [Route("cad/cad-data/report/zip")]
+        public async Task<IActionResult> TaskDataDownload(string siteId, string activityInstanceCode)
+        {
+            var headers = HttpContext.Request.Headers;
+            var projectCode = headers["project-code"].ToString();
+            var projectId = headers["project-id"].ToString();
+            var userInfo = headers["user-info"].ToString();
+
+            _logger.LogInformation(
+@"Download task data, projectCode = {}, projectId = {}, userInfo = {},
+siteId = {}, activityInstanceCode = {}",
+            projectCode, projectId, userInfo, siteId, activityInstanceCode);
+
+            if (string.IsNullOrWhiteSpace(projectCode) ||
+            string.IsNullOrWhiteSpace(projectId) ||
+            string.IsNullOrWhiteSpace(userInfo) ||
+            userInfo.Substring(1, 9) != "\"account\"")
+                return new JsonResult(new { ErrorCode = 404, Msg = "account error" });
+
+            if (string.IsNullOrWhiteSpace(siteId) ||
+            string.IsNullOrWhiteSpace(activityInstanceCode))
+                return new JsonResult(new { ErrorCode = 404, Msg = "siteId or activityInstanceCode is empty" });
+
+            return File(await System.IO.File.ReadAllBytesAsync("./TestData/taskdata.zip"),
+            "applicaton/zip", Guid.NewGuid() + ".zip");
+        }
+
+        [HttpGet]
+        [Route("cad/download-cad-block")]
+        public async Task<IActionResult> CadDataDownload()
+        {
+            var headers = HttpContext.Request.Headers;
+            var projectCode = headers["project-code"].ToString();
+            var projectId = headers["project-id"].ToString();
+            var userInfo = headers["user-info"].ToString();
+
+            _logger.LogInformation("Download cad block data, projectCode = {}, projectId = {}, userInfo = {}",
+            projectCode, projectId, userInfo);
+
+            if (string.IsNullOrWhiteSpace(projectCode) ||
+            string.IsNullOrWhiteSpace(projectId) ||
+            string.IsNullOrWhiteSpace(userInfo) ||
+            userInfo.Substring(1, 9) != "\"account\"")
+                return new JsonResult(new { ErrorCode = 404, Msg = "account error" });
+
+            return File(await System.IO.File.ReadAllBytesAsync("./TestData/caddata.zip"),
+            "applicaton/zip", Guid.NewGuid() + ".zip");
+        }
+    }
+}
